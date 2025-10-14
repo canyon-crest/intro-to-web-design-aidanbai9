32ac0dd53244113146b80b42db06687c8dd7b9bb
aidanbai9 <aidanbai9@gmail.com>
2025-10-14 20:50:55 +0000

    end of day 1

---
+++ sigma.cpp
@@ -0,0 +1,13 @@
+#include "bits/extc++.h"
+
+using namespace std;
+
+void solve(){
+    cout<<"SIGMA\n";
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(ios::failbit);
+    solve();
+}
